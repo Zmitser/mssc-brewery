c05d9b700ad79fac21d4c5a0ec180faa9497b6d9
Dmitriy Borisovets <gagat@tut.by>
2019-10-13 22:16:17 +0300

    add log for update

--- src/main/kotlin/by/zmitser/service/BeerServiceImpl.kt
+++ src/main/kotlin/by/zmitser/service/BeerServiceImpl.kt
@@ -3,16 +3,17 @@ package by.zmitser.service
 import by.zmitser.web.model.BeerDto
 import mu.KotlinLogging
 import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import java.util.*
+
 import javax.inject.Singleton
 
 @Singleton
-class BeerServiceImpl(private val logger: Logger = KotlinLogging.logger {  }) : BeerService {
-
+class BeerServiceImpl() : BeerService {
+    private val logger: Logger = KotlinLogging.logger { }
     override fun save(beer: BeerDto): BeerDto = BeerDto(UUID.randomUUID(), "", "");
     override fun update(id: UUID, beer: BeerDto): BeerDto {
-        TODO("add real implementation")
+        logger.info("Update a beer...")
+        return BeerDto()
     }
 
     override fun findOne(id: UUID): BeerDto = BeerDto(UUID.randomUUID(), "Galaxy Cat", "Pale Ale")
